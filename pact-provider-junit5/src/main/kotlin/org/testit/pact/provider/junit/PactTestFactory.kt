4fa2a5ba32d20aa6325510fc5a7e3a283b0dd280
Stefan Ludwig <stefan.ludwig@novatec-gmbh.de>
2018-02-27 22:58:34 +0100

    refactored PatTestFactory class

--- pact-provider-junit5/src/main/kotlin/org/testit/pact/provider/junit/PactTestFactory.kt
+++ pact-provider-junit5/src/main/kotlin/org/testit/pact/provider/junit/PactTestFactory.kt
@@ -5,16 +5,30 @@ import org.junit.jupiter.api.DynamicTest.dynamicTest
 import org.testit.pact.provider.http.RequestResponsePacts
 import org.testit.pact.provider.message.MessagePacts
 
-object PactTestFactory {
+class PactTestFactory {
 
-    fun createTests(pacts: RequestResponsePacts, consumerFilter: String? = null, callbackHandler: Any? = null): List<DynamicTest> {
-        return pacts.createExecutablePacts(consumerFilter, callbackHandler)
-                .map { dynamicTest(it.name, it.executable) }
-    }
+    /*
+     * Exposes the static factory for Java clients and still makes it usable for
+     * Kotlin clients as well.
+     *
+     * If this file were an `object` instead of a `class`, it would expose an ugly
+     * static `INSTANCE` field for Java clients. Since this is an API class, doing
+     * it this way instead of a more elegant Kotlin purist way is acceptable.
+     */
+    companion object {
 
-    fun createTests(pacts: MessagePacts, consumerFilter: String? = null, callbackHandler: Any): List<DynamicTest> {
-        return pacts.createExecutablePacts(consumerFilter, callbackHandler)
-                .map { dynamicTest(it.name, it.executable) }
+        @JvmStatic
+        fun createTests(pacts: RequestResponsePacts, consumerFilter: String? = null, callbackHandler: Any? = null): List<DynamicTest> {
+            return pacts.createExecutablePacts(consumerFilter, callbackHandler)
+                    .map { dynamicTest(it.name, it.executable) }
+        }
+
+        @JvmStatic
+        fun createTests(pacts: MessagePacts, consumerFilter: String? = null, callbackHandler: Any): List<DynamicTest> {
+            return pacts.createExecutablePacts(consumerFilter, callbackHandler)
+                    .map { dynamicTest(it.name, it.executable) }
+        }
+
     }
 
 }
